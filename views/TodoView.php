7a4eb3809201b4362a6e2b03fbd27ed8fba0346c
Daniel L. Tobing <ninacinta206@gmail.com>
2025-12-04 13:31:42 +0700

    push

--- views/TodoView.php
+++ views/TodoView.php
@@ -8,12 +8,12 @@ $search = $_GET['search'] ?? '';
 <html>
 <head>
     <title>Aplikasi Todolist Niel | Modern & Mewah</title>
-    <link rel="icon" type="image/png" href="/assets/favicon.png">
+    
     <link href="/assets/vendor/bootstrap-5.3.8-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="/assets/vendor/bootstrap-icons-1.13.1/bootstrap-icons.min.css" rel="stylesheet" />
     
     <link href="/assets/css/style.css" rel="stylesheet" />
-   <link rel="icon" type="image/x-icon" href="/public/assets/img/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
 
     <style>
        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
